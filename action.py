5525f2225e836a61e7d5c82f582b1577bc97eb69
COSTFULS520 <111938754+COSTFULS520@users.noreply.github.com>
2023-05-22 19:19:14 +0800

    Update action.py

--- action.py
+++ action.py
@@ -76,8 +76,8 @@ def print_msg(msg: str = "", isFirstLine: bool = False) -> str:
 
 if __name__ == '__main__':
     # 多账号用单个空格 间隔开
-    USERNAME = ''
-    PASSWORD = ''
+    USERNAME = 18085721099
+    PASSWORD = 520CAIcai+
     
     # 启用邮箱发信
     mail_enable = True
